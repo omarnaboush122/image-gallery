9a4737991d8f21f1f5e9d24d706dc6d13c5d1c9a
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-04 18:04:34 -0800

    add header

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import { nanoid } from 'nanoid';
 const App = () => {
 
   return (
-    <div>
-    hello world
+    <div className='container '>
+      <h1 className="mt-3">My Budget Planner</h1>
+      
     </div>
   );
 };
