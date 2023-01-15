da7c306823473f99ea7de971c7342023b84591c1
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-15 11:01:38 -0800

    map over the fetched data array

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import ImageCard from "./components/ImageCard";
 
 const App = () => {
   const [images, setImages] = useState([]);
@@ -11,44 +12,20 @@ const App = () => {
     )
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
+        setImages(data.hits);
         setLoading(false);
       })
       .catch(err => console.log(err))
   }, []);
 
+  const allImages = images.map(image => (
+    <ImageCard key={image.id} image={image}/>
+  ))
+
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by joe doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views:</strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads:</strong>
-            300
-          </li>
-          <li>
-            <strong>Likes:</strong>
-            400
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>{" "}
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {allImages}
       </div>
     </div>
   );
