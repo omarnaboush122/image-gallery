1ed503a8440658f78abec4143dc3bce41f2d6484
omarnaboush122 <omarnaboush89@gmail.com>
2023-01-15 09:23:07 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import { nanoid } from 'nanoid';
 
 
 const App = () => {
 
   return (
-    <div className='container '>
-      <h1 className="mt-3">My Budget Planner</h1>
-      
+    <div>
+      hello
     </div>
   );
 };
